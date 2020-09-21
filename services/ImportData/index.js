ff4f0dbb22e2fda18ec152d607de960733f57470
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2020-09-21 17:31:44 -0300

    using just one collection 'Histories'

--- services/ImportData/index.js
+++ services/ImportData/index.js
@@ -2,49 +2,47 @@ const fetch = require('node-fetch');
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = async function (context, myTimer) {
-    let headers = {
-        'Authorization': `Bearer ${process.env["ClasOfClansApiToken"]}`
-    };
-    
-    const clanUri = `https://api.clashofclans.com/v1/clans/${encodeURIComponent(process.env['ClanTag'])}`;
-    let result = await fetch(clanUri, { headers });
-    let clan = await result.json();
+    try {
+        let headers = {
+            'Authorization': `Bearer ${process.env["ClasOfClansApiToken"]}`
+        };
 
-    const client = new MongoClient(process.env["MongoDbConnectionString"], { useNewUrlParser: true });
-    client.connect(async err => {
+        const clanUri = `https://api.clashofclans.com/v1/clans/${encodeURIComponent(process.env['ClanTag'])}`;
+        context.log('Fetching clan');
+        let result = await fetch(clanUri, { headers });
+        let clan = await result.json();
+        //context.log(clan);
+        context.log('clan name: ' + clan.name);
+
+        const client = new MongoClient(process.env["MongoDbConnectionString"], { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+
         var timeStamp = new Date().toISOString();
         const db = client.db("clash");
-        db.collection('ClansHistories').insertOne({
-            at: timeStamp,
-            tag: clan.tag,
-            clan: {
-                tag: clan.tag,
-                memberList: clan.memberList.map(m => ({
-                    tag: m.tag,
-                    name: m.name,
-                    clanRank: m.clanRank
-                }))
-            }
-        });
 
-        let playersHistories = [];
+        let history = {
+            at: timeStamp,
+            clanTag: clan.tag,
+            clanName: clan.name,
+            players: []
+        };
+
         for (let member of clan.memberList) {
+            context.log(`getting player: ${member.name} ${member.tag}`);
             result = await fetch(`https://api.clashofclans.com/v1/players/${encodeURIComponent(member.tag)}`, { headers });
             const player = await result.json();
 
-            playersHistories.push({
-                at: timeStamp,
+            history.players.push({
                 tag: player.tag,
-                clangTag: clan.tag,
-                player: {
-                    tag: player.tag,
-                    name: player.name,
-                    achievements: player.achievements.filter(a => a.name == 'Games Champion')
-                }
+                name: player.name,
+                achievements: player.achievements.filter(a => a.name == 'Games Champion')
             });
         }
-        db.collection('PlayersHistories').insertMany(playersHistories);
+        await db.collection('Histories').insertOne(history);
 
-        client.close();
-    });
+        await client.close();
+        context.log(`import succeeded`);
+    } catch (err) {
+        context.log(err);
+    }
 };
